496fece7c6a9b37dafb6e47e65ffced8ec764ae6
Brendan <brendanalex212@gmail.com>
2024-06-07 15:32:22 +0700

    Initial Commit

---
+++ Nekoma/NekomaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NekomaApp.swift
+//  Nekoma
+//
+//  Created by Brendan Alexander Soendjojo on 07/06/24.
+//
+
+import SwiftUI
+
+@main
+struct NekomaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
